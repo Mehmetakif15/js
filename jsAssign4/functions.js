8b8eed9398b34ccff3e638570a3314591d52817f
mehmet akif <macguld15@gmail.com>
2022-12-26 00:45:27 +0300

    added two answers

--- jsAssign4/functions.js
+++ jsAssign4/functions.js
@@ -17,6 +17,19 @@ const kareAl = function (kenar) {
 const kenar = Number(prompt("Karenin bir kenar uzunlugunu giriniz:"));
 console.log(kareAl(kenar));
 
+const kupAl = function (knr) {
+  return knr ** 3;
+};
+const knr = Number(prompt("Kupun bir kenar uzunlugunu giriniz:"));
+console.log(kupAl(knr));
+
+const usAl = function (us) {
+  return sayi ** us;
+};
+const sayi = Number(prompt("Ussunu almak istediginiz sayiyi giriniz:"));
+const us = Number(prompt("Ussu giriniz:"));
+console.log(usAl(us));
+
 // * Q3 : Yil degerlerini parametre olarak alan ve bu yilin artik yil olup olmadigini hesaplayarak sonucu ana programa  donduren fonksiyonu function-expression yontemi ile yaziniz. Not : Yil 4'e tam bolunuyorsa ve (100'e tam bolunmuyorsa veya 400'e tam bolunuyorsa) artik yildir aksi takdirde degildir.
 
 const artik = function (yil) {
