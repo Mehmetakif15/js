87351f7213def79fb18798e94369423e8d192324
mehmet akif <macguld15@gmail.com>
2023-01-12 23:00:54 +0300

    fns

---
+++ js14/3-createNode.js
@@ -0,0 +1,48 @@
+console.log("**** CREATE NODE ****");
+
+const newPar = document.createElement("p");
+const text = document.createTextNode("Bu bir p elementidir.");
+newPar.appendChild(text);
+console.log(newPar);
+
+const h2 = document.querySelector(".item-list h2");
+h2.after(newPar);
+// h2.before(newPar);
+
+//? INPUT value
+const myInput = document.getElementById("input");
+console.log(myInput.value);
+
+//? id, class
+newPar.id = "par";
+newPar.className = "center red";
+
+//? setAttribute
+newPar.setAttribute("id", "para"); //?alternative
+
+//? classList
+newPar.classList.add("bg-tomato", "left");
+newPar.classList.remove("left");
+
+if (!newPar.classList.contains("blue")) {
+  newPar.classList.add("blue");
+} else {
+  newPar.classList.add("orange");
+}
+
+//? toggle
+newPar.classList.toggle("white"); //? white class'i yoksa ekler veya varsa kaldirir.
+
+//? getAttribute()
+console.log(newPar.getAttribute("class"));
+console.log(newPar.getAttribute("id"));
+console.log(myInput.getAttribute("name"));
+console.log(myInput.getAttribute("type"));
+
+//? alternative
+console.log(myInput.id);
+console.log(newPar.className);
+
+//? INNER HTML (CIDDI GUVENLIK ACIKLARI VAR)
+const newDiv = document.querySelector("#new-div");
+newDiv.innerHTML += `<li id="go" class="red">Go</li>`;
