33a66ee2e4e116d1f49831a83fa7f7089ed88040
mehmet akif <macguld15@gmail.com>
2023-01-14 00:00:07 +0300

    yea

---
+++ jsEmailValidation/email.js
@@ -0,0 +1,11 @@
+function validateEmail(email) {
+  const regex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (regex.test(email) && email.split(".")[1].length <= 3) {
+    return "Valid Email";
+  } else {
+    return "Invalid Email";
+  }
+}
+
+console.log(validateEmail("example@example.com"));
