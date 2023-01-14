b1824f543053dae3a83f6e730835b5d94f73810c
mehmet akif <macguld15@gmail.com>
2023-01-14 22:42:41 +0300

    fns

--- jsEmailValidation/email.js
+++ jsEmailValidation/email.js
@@ -1,11 +1,20 @@
+//*** Purpose of the this coding challenge is to write a code that given email adresses, returns the emails valid or invalid.
+
+//*** Valid email addresses must follow these rules:
+//*** It must have the username@websitename.extension format type.
+//*** The maximum length of the extension is 3.
+
 function validateEmail(email) {
-  const regex =
-    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  if (regex.test(email) && email.split(".")[1].length <= 3) {
-    return "Valid Email";
+  // Regular expression to check for the format of the email address
+  const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/;
+  // Check if the email address matches the regular expression
+  if (regex.test(email)) {
+    console.log("valid");
+    return true;
   } else {
-    return "Invalid Email";
+    console.log("invalid");
+    return false;
   }
 }
-let mail = prompt("Please enter your email");
-console.log(validateEmail(mail));
+let email = prompt("Please enter your email");
+validateEmail(email);
